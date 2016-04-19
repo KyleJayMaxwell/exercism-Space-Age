c60652484ae23340168b038b47c3626c57dbb85c
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-04-19 09:42:20 -0600

    finished the exercism

--- space-age.js
+++ space-age.js
@@ -8,5 +8,46 @@ SpaceAge.prototype.onEarth = function() {
   return round;
 };
 
+SpaceAge.prototype.onMercury = function() {
+  var mercury = (this.seconds / 7600525);
+  var round = Math.round(mercury * 100) / 100
+  return round;
+};
+
+SpaceAge.prototype.onVenus = function() {
+  var venus = (this.seconds / 19411026);
+  var round = Math.round(venus * 100) / 100
+  return round;
+};
+
+SpaceAge.prototype.onMars = function() {
+  var mars = (this.seconds / 59359776);
+  var round = Math.round(mars * 100) / 100
+  return round;
+};
+
+SpaceAge.prototype.onJupiter = function() {
+  var jupiter = (this.seconds / 374222565);
+  var round = Math.round(jupiter * 100) / 100
+  return round;
+};
+
+SpaceAge.prototype.onSaturn = function() {
+  var saturn = (this.seconds / 928792569);
+  var round = Math.round(saturn * 100) / 100
+  return round;
+};
+
+SpaceAge.prototype.onUranus = function() {
+  var uranus = (this.seconds / 2652994591);
+  var round = Math.round(uranus * 100) / 100
+  return round;
+};
+
+SpaceAge.prototype.onNeptune = function() {
+  var neptune = (this.seconds / 5196280668);
+  var round = Math.round(neptune * 100) / 100
+  return round;
+};
 
 module.exports = SpaceAge;
